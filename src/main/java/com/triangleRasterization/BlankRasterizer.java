8c56d3d9e0b94cd009975900cff2369733696d8b
Fiecher <stepanbelebezev@gmail.com>
2024-10-16 16:57:00 +0300

    Initial commit

---
+++ src/main/java/com/triangleRasterization/BlankRasterizer.java
@@ -0,0 +1,5 @@
+package src.main.java.com.triangleRasterization;
+
+public interface BlankRasterizer {
+
+}
---
+++ src/main/java/com/triangleRasterization/Main.java
@@ -0,0 +1,4 @@
+package src.main.java.com.triangleRasterization;
+
+public class Main {
+}
